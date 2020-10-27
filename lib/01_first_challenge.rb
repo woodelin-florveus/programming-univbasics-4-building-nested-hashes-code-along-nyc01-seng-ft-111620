868b5f4944e822c9577936ec298d0c183b8117cc
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-27 19:19:12 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,7 +1,17 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {},
-     :capulet => {}
+     :montague => {
+       :patriarch => {},
+       :matriarch => {},
+       :hero => {},
+       :hero_friends => []
+     },
+     :capulet => {
+       :patriarch => {},
+       :matriarch => {},
+       :heroine => {},
+       :heroine_friends => []
+     }
 
    }
 end
