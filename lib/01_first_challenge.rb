6048d673b5b07915b93d6be8fcc09f8e302d4330
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-27 19:21:52 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
    epic_tragedy = {
      :montague => {
        :patriarch => {:name => "Lord Montague" :age => "53"},
-       :matriarch => {:name => "Lady Montague" :age => "53"},
+       :matriarch => {:name => "Lady Montague" :age => "54"},
        :hero => {},
        :hero_friends => []
      },
