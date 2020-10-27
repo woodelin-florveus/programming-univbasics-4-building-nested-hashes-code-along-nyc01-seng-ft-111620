dbb613c4ce0b682137577fc062e124d783cf5c53
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-27 19:35:58 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -4,7 +4,18 @@ def first_challenge
        :patriarch => {:name => "Lord Montague" :age => "53"},
        :matriarch => {:name => "Lady Montague" :age => "54"},
        :hero => {:name => "Romeo" :age => "15" :status => "alive"},
-       :hero_friends => [:name => "Benvolio" ,:age => 17 , :attitude => "worried"]
+       :hero_friends => [
+         :first_friend => {
+           :name => "Benvolio" ,
+           :age => 17 ,
+           :attitude => "worried"
+         },
+         :second_friend => {
+           :name => "Mercutio" ,
+           :age => 18 ,
+           :attitude => "hot-headed"
+         }
+       ]
      },
      :capulet => {
        :patriarch => {:name => "Lord Capulet" :age => "50"},
